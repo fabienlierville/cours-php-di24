fda811ac309c2f3255208d6ec0e9106bdf02b8b9
fabien <flierville@cesi.fr>
2025-12-15 10:25:24 +0100

    API

--- src/Controller/ApiArticleController.php
+++ src/Controller/ApiArticleController.php
@@ -21,4 +21,37 @@ class ApiArticleController{
         return json_encode($articles);
 
     }
+
+    public function add(){
+        if($_SERVER['REQUEST_METHOD'] != 'POST'){
+            header("HTTP/1.1 405 Method Not Allowed");
+            return json_encode([
+                'success' => false,
+                'message' => 'Method Not Allowed'
+            ]);
+        }
+
+        //Récupération des données du Body
+        $data = json_decode(file_get_contents("php://input"));
+
+        if(empty($data->Titre) || empty($data->Auteur) || empty($data->Description)){
+            header("HTTP/1.1 400 Bad Request");
+            return json_encode([
+                'success' => false,
+                'message' => 'Il manque des données obligatoires'
+            ]);
+        }
+        // Création de l'article + insert en BDD
+        $article = new Article();
+        $article->setTitre($data->Titre)
+            ->setAuteur($data->Auteur)
+            ->setDescription($data->Description)
+            ->setDatePublication(new \DateTime($data->DatePublication));
+        $id = Article::SqlAdd($article);
+        return json_encode([
+            'success' => true,
+            'id' => $id,
+            'message' => 'Article ajouté avec succès'
+        ]);
+    }
 }
